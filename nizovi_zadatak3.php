06270f3fa571b438a316569a4dbedf13726e5331
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-14 23:24:30 +0100

    nizovi zadatak 3

---
+++ nizovi_zadatak3.php
@@ -0,0 +1,9 @@
+<?php
+
+// Zadatak 3
+// Data je jedna liste
+// Dodati broj 10 na kraju liste zatim odstampati listu
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+$numbers[] = "10";
+print_r($numbers);

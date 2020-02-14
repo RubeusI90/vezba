cfaf8017a62a0fdf4051a24502896e4132e84b78
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-14 12:14:28 +0100

    dodao sam zadatak1

---
+++ zadatak1.php
@@ -0,0 +1,20 @@
+<?php
+
+// Zadatak 1
+// Imas broj i listu
+// Ako je broj(number) veci nego broj elemenata u listi
+// Napisi poruku: BROJ JE VELIKI
+// Ako nije napisi poruku: BROJ JE MALI
+// Moras da nadjes funckiju online koja ti kaze duzinu liste(broj elemenata u listi)
+$number = 5;
+$names = ['Dusan', 'Ivan', 'Nemanja', 'Nikola'];
+
+
+$broj_u_listi = sizeof($names);
+//echo $broj_u_listi;
+
+if ($number >= $broj_u_listi) {
+    echo "Broj je veliki.";
+} elseif ($number <= $broj_u_listi) {
+    echo "Broj je mali.";
+}

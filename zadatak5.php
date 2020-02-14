9696d2cf076f3c43eaced552180916cf1d708efa
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-14 21:24:04 +0100

    prepravljeno zadatak5

--- zadatak5.php
+++ zadatak5.php
@@ -6,12 +6,12 @@
 // Ako je ispor 3 godine treba napisati: karta je 100 dinara
 // Ako je ispor 18 a vise od 2 godine treba napisati: karta je 400 dinara
 // Ako je 18 ili vise godina treba napisati: karta je 600 dinara
-$age = 17;
+$age = 18;
 
 if ($age < 3) {
     echo "Karta je 100 dinara.";
 } elseif ($age < 18 & $age > 2) {
     echo "Karta je 400 dinara.";
-} elseif ($age > 18) {
+} elseif ($age >= 18) {
     echo "Karta je 600 dinara.";
 }

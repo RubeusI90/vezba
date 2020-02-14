432e49b5afa44f580cdeb62835e936656b89a81b
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-14 23:29:29 +0100

    nizovi zadatak 4

---
+++ nizovi_zadatak4.php
@@ -0,0 +1,11 @@
+<?php
+
+// Zadatak 4
+// Dat je asocijativni niz
+// Odstampati poruku: Dusan is 40 years old
+$info = [
+    'name' => 'Dusan',
+    'age' => 40,
+];
+
+echo $info['name'] . " is " . $info['age'] . " years old.";

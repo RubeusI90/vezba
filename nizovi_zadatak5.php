2134877998c9888b6ede1ac91fb0ab2ee0abe312
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-14 23:32:50 +0100

    nizovi zadatak 5

---
+++ nizovi_zadatak5.php
@@ -0,0 +1,12 @@
+<?php
+
+// Zadatak 5
+// Data je asocijativni niz
+// Dotati key-value: France Paris
+$countries = [
+    'Japan' => 'Kyoto',
+    'Serbia' => 'Nis'
+];
+
+$countries["France"] = "Paris";
+print_r($countries);

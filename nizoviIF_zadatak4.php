617aca829e82f84d9802f0ecba03628bb62678b7
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-15 11:10:36 +0100

    nizovi IF zadatak 4

---
+++ nizoviIF_zadatak4.php
@@ -0,0 +1,8 @@
+<?php
+
+// ZADATAK 4
+// Data je jedna liste
+// Otkucati svako ime u novom redu ali koristiti samo jedan echo
+$n = ['Dusan', 'Ivan', 'Nemanja'];
+
+echo $n["0"] . "<br>" . $n["1"] . "<br>" . $n["2"];

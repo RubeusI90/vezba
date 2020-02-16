1d0055c3d48fd8bae9416f79b2d53a04949847a8
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-16 20:32:24 +0100

    nizovi IF zadatak 9

---
+++ nizoviIF_zadatak9.php
@@ -0,0 +1,13 @@
+<?php
+
+// ZADATAK 9
+// Data su dva aocijativna niza
+// Ako je vrednost user iz prvog niza prazna ili je 
+// vrednost user drugog niza shinobi
+// Odstampati: At least one exists
+$isEmpty1 = ['user' => ''];
+$isEmpty2 = ['user' => 'SinnerHell'];
+
+if (empty($isEmpty1["user"]) || $isEmpty["user"] == "shinobi") {
+    echo "At least one exist.";
+}

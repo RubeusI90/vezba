81dbed02c98e52eb2498c63f2d7ab1c008eb67b2
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-18 00:19:02 +0100

    user input zadatak 2

--- userinput_zadatak3.php
+++ userinput_zadatak3.php
@@ -1,22 +1,22 @@
 <?php
-// Zadatak 2
+// Zadatak 3
 // Napraviti formu sa dva input taga
 // username i email
 // Kada idemo submit odstampati: Welcome username
+// Napraviti da ne izadje greska unindentified index
+// kada prvi put dodjemo na stranicu
 ?>
 
 <form name="form1" method="POST" action="">
     User name: <br>
     <input type="text" name="username"><br>
     Email: <br>
-    <input type="text" name="email"><br><br>
+    <input type="text" name="email"><br>
     <input type="submit" name="button" value="Send"><br>
 </form>
 
 <?php
 
 if (isset($_POST["button"])) {
-    $username = $_POST["username"];
-    echo "Wlcome " . $username;
-}
-?>
+    $welcome = $_POST["username"];
+    echo "Welcome " . $welcome;

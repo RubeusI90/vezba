e4df4cf8fc4f4e6f5c506411ec3bdcd63b075093
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-18 00:01:54 +0100

    user input zadatak 2

---
+++ userinput_zadatak2.php
@@ -0,0 +1,20 @@
+<?php
+// Zadatak 2
+// Napraviti formu sa dva input taga
+// username i email
+// Kada idemo submit odstampati: Welcome username
+?>
+
+<form name="form1" method="POST" action="">
+    User name: <br>
+    <input type="text" name="username"><br>
+    Email: <br>
+    <input type="text" name="email"><br><br>
+    <input type="submit" name="button" value="Send"><br>
+</form>
+
+<?php
+
+if (isset($_POST["button"])) {
+    echo "Welcome user.";
+}

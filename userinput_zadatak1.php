14ed5823be9bb80a50bf1c64e0dc7cbbea344b44
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-17 01:48:30 +0100

    user input zadatak 1

---
+++ userinput_zadatak1.php
@@ -0,0 +1,18 @@
+<?php
+
+// Zadatak 1
+// Napraviti formu sa dva input taga
+// username i email
+// Kada idemo submit odstampati celu $_POST listu
+
+?>
+
+<form action="" method="post">
+    <input type="text" name="username">
+    <br>
+    <br>
+    <input type="text" name="email">
+    <br>
+    <br>
+    <input type="submit" name="button" value="SEND">
+</form>

084fe2d44464f188c77233d1b3ce4274ea2d454e
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-19 01:28:14 +0100

    zadatak 4

--- zadatak4.php
+++ zadatak4.php
@@ -5,16 +5,16 @@
 // Napraviti if elseif za kalkulator u zavisnosti od vrednosi operanda
 $num1 = 10;
 $num2 = 5;
-$operand = '+'; # operand je sada '+' ali treba napisati za sve moguce vrednosti.
+$operand = '*'; # operand je sada '+' ali treba napisati za sve moguce vrednosti.
 // Ako je $operand = '+' treba izaci 15
 // Ako je $operand = '-' treba izaci 5 itd.
 
-if ($operand = "+") {
+if ($operand == "+") {
     echo $num1 + $num2;
-} elseif ($operand = "-") {
+} elseif ($operand == "-") {
     echo $num1 - $num2;
-} elseif ($operand = "*") {
+} elseif ($operand == "*") {
     echo $num1 * $num2;
-} elseif ($operand = "/") {
+} elseif ($operand == "/") {
     echo $num1 / $num2;
 }

4b93a75a8723c682253ec7252a4bda59b89fad02
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-25 10:32:56 +0100

    funkcije zadatak 1

---
+++ funkcije/funkcije_zadatak1.php
@@ -0,0 +1,10 @@
+<?php
+// Zadatal 1
+
+// Definisati funkciju 'getMsg' koja nece imati ni jedan parametar
+// Funkcija treba da vrati string 'Hello from msg function'
+
+function getMsg()
+{
+    return "Hello";
+}

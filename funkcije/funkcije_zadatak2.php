38492c50f3ba416744614ad3233053984f60dbce
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-25 11:13:58 +0100

    funkcije zadatak 2

---
+++ funkcije/funkcije_zadatak2.php
@@ -0,0 +1,19 @@
+<?php
+
+// Zadatak 2
+
+// Definisati funkciju 'add' sa dva parametra
+// Funkcija treba da vrati zbir dva broja koja unosimo
+// Zatim pozvati funkciju sa datim vrednostima $number1, $number2
+
+$number1 = 10;
+$number2 = 4;
+
+function add($number1, $number2)
+{
+    $total = $number1 + $number2;
+    return $total;
+}
+
+$num1 = add(10, 22);
+echo $num1;

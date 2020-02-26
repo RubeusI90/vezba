846ba0234ceeea8f3a7b69a901f76d1b6539e0fa
RubeusI04 <storm.raiders.i04@gmail.com>
2020-02-26 11:35:22 +0100

    funkcije zadatak 5

---
+++ funkcije/funkcije_zadatak5.php
@@ -0,0 +1,39 @@
+<?php
+// Zadatak 5
+
+// Definisati funkciju 'calculator'
+// Funkcija prima 3 parametra: $num1, $num2, $operation
+// $operation parametar ce prihvatati +, -, /, *
+// Treba napisati unutar funkcije if elif tako da...
+//       u zavisnosti od $operation funkcija vraca rezultat
+// Dat je $num1 i $num2
+
+$num1 = 12;
+$num2 = 3;
+
+// Pozvati funkciju 4 puta
+// Uvek uneti date $num1 i $num2 kada se zove funkcija
+// Za $operation uneti sve vrednosti po jednom
+// Primer: function($num1, $num2, '+')
+// Odstampati svaki rezultat
+
+function calculator($num1, $num2, $operation)
+{
+    if ($operation == "+") {
+        echo $num1 + $num2;
+    } elseif ($operation == "-") {
+        echo $num1 - $num2;
+    } elseif ($operation == "/") {
+        echo $num1 / $num2;
+    } elseif ($operation == "*") {
+        echo $num1 * $num2;
+    }
+}
+
+$operation1 = calculator($num1 = 12, $num2 = 3, "+");
+echo "<br>";
+$operation2 = calculator($num1 = 12, $num2 = 3, "-");
+echo "<br>";
+$operation3 = calculator($num1 = 12, $num2 = 3, "/");
+echo "<br>";
+$operation4 = calculator($num1 = 12, $num2 = 3, "*");

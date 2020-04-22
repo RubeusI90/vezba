24eac9698885f06c83bbbf483d3690effa1133c6
RubeusI04 <storm.raiders.i04@gmail.com>
2020-04-22 21:22:56 +0200

    zadatak for_zadatak2

---
+++ for/for_zadatak2.php
@@ -0,0 +1,17 @@
+<?php
+
+// Zadatak 2
+
+// Izracunati sumu prva 4 broja (1+2+3+4)
+/*
+Output:
+    10
+*/
+
+$num1 = 1;
+$num4 = 4;
+for ($i = $num1; $i <= $num4; $i++) {
+    $sum += $i;
+}
+
+echo $sum;
